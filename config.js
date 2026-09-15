5c866ea6bb74c2466f4fffbe6692f1e4c36ac686
wwwrwww65-netizen <wwwrwww65@gmail.com>
2026-09-15 10:41:17 +0300

    اصلاح التحسين

--- config.js
+++ config.js
@@ -1,14 +1,27 @@
 window.siteConfig = {
+    // 1. اسم الشبكة والعناوين والهوية
     "siteName": "يمن نت",
-    "subDecText": "للإنترنت اللاسلكي",
-    "welcomeMessage": "مرحبا بكم في شبكة الأولى نت اللاسلكية",
+    "network-name": "يمن نت",
+    "subDecText": "لخدمات الإنترنت اللاسلكي",
+    "welcomeMessage": "مرحبا بكم في شبكة يمن نت اللاسلكية",
     "welcomeMessageV": true,
     "erbV": false,
-    "textSlider1": "بسم الله الرحمن الرحيم { قل للمؤمنين يغضوا من أبصارهم ويحفظوا فروجهم ذلك أزكى لهم إن الله خبير بما يصنعون } (النور:30)",
-    "supportPhone": "733088846",
-    "whatsappPhone": "967779744206",
+
+    // 2. الشريط الإخباري والتراحيب
+    "news-line": "مرحبا  بكم  في شبكتنا  تذكر ان الله ينظر إليك فلا تجعله أهون الناظرين إليك * نسعى لتقديم أفضل خدمة إنترنت سريعة ومتميزة ",
+    "textSlider1": "مرحبا  بكم  في شبكتنا  تذكر ان الله ينظر إليك فلا تجعله أهون الناظرين إليك * نسعى لتقديم أفضل خدمة إنترنت سريعة ومتميزة ",
+
+    // 3. أرقام خدمة العملاء والواتساب
+    "supportPhone": "770807056",
+    "service-number": "770807056",
+    "whatsappPhone": "967770807056",
     "whatsappMsg": "مرحبا خدمة عملاء شبكة {network-name} اللاسلكية",
+
+    // 4. خيارات سرعة الإنترنت
     "fixedSpeedCardPrefixes": ["2", "3"],
+    "speed-select": 0,
+    "speed-var": "speed",
+    "speed-option": "",
     "speedOptions": [
         {
             "value": "512K/1M",
@@ -41,65 +54,172 @@ window.siteConfig = {
             "selected": false
         }
     ],
+
+    // 5. صور السلايدر الإعلاني
     "imageCount": "7",
     "imageV": true,
+
+    // 6. باقات وأسعار الكروت (مطابقة لمشروع 770807777 السابق)
     "packages": [
         {
-            "vl": "2 أيام",
+            "price": "100 ريال",
+            "time": "3 ساعات",
+            "size": "400 ميجا",
+            "vl": "30 يوم"
+        },
+        {
+            "price": "100 ريال",
+            "time": "3 ساعات",
+            "size": "مفتوح",
+            "vl": "30 يوم"
+        },
+        {
+            "price": "200 ريال",
+            "time": "7 ساعات",
+            "size": "800 ميجا",
+            "vl": "30 يوم"
+        },
+        {
+            "price": "250 ريال",
             "time": "مفتوح",
             "size": "1 جيجا",
-            "price": "200 ريال"
+            "vl": "يوم"
         },
         {
-            "vl": "3 أيام",
-            "time": "مفتوح",
-            "size": "1.5 جيجا",
-            "price": "300 ريال"
+            "price": "300 ريال",
+            "time": "10 ساعات",
+            "size": "مفتوح",
+            "vl": "30 يوم"
         },
         {
-            "vl": "6 أيام",
+            "price": "500 ريال",
+            "time": "20 ساعة",
+            "size": "2 جيجا",
+            "vl": "30 يوم"
+        },
+        {
+            "price": "1000 ريال",
             "time": "مفتوح",
             "size": "4 جيجا",
-            "price": "500 ريال"
-        },
-        {
-            "vl": "10 أيام",
-            "time": "مفتوح",
-            "size": "8 جيجا",
-            "price": "1000 ريال"
-        },
-        {
-            "vl": "شهر",
-            "time": "مفتوح",
-            "size": "30 جيجا",
-            "price": "5000 ريال"
+            "vl": "30 يوم"
         }
     ],
-    "offersTitle": "عروض ومميزات الشبكة الحصرية 🔥",
-    "offers": "انترنت سريع ",
-    "offersBtnText": "📺 مشاهدة البث المباشر والمباريات",
-    "offersBtnUrl": "https://kor.fntvs.net/channel.php",
-    "offersBtnV": false,
-    "salesPoints": [
-        "بقالة صلاح موسى",
-        "بقالة مستشار مصطفى",
-        "بقالة عبده الكافي",
-        "بقالة عبدالله قايد",
-        "بقالة نافع مهيوب",
-        "1 القحطاني تيليكوم (تطبيق لشراء الكروت)",
-        "2 رصيدي تيليكوم (تطبيق لشراء الكروت)",
-        "3 الشعبي اونلاين (تطبيق لشراء الكروت)"
+    // مطابقة مفاتيح profiles الخاصة بنظام الميكروتيك والقوالب
+    "profiles": [
+        {
+            "price": "100  ريال",
+            "time": "3ساعات",
+            "transfer": "400 ميجا",
+            "validity": "30 أيام"
+        },
+        {
+            "price": "100  ريال",
+            "time": "3 ساعات",
+            "transfer": "مفتوح",
+            "validity": "30 أيام"
+        },
+        {
+            "price": "200  ريال",
+            "time": "7 ساعات",
+            "transfer": "800 ميجا",
+            "validity": "30 أيام"
+        },
+        {
+            "price": "250  ريال",
+            "time": "مفتوح ",
+            "transfer": "1 جيجا",
+            "validity": "يوم"
+        },
+        {
+            "price": "300  ريال",
+            "time": "10 ساعات",
+            "transfer": "مفتوح",
+            "validity": "30 أيام"
+        },
+        {
+            "price": "500 ريال",
+            "time": "20ساعه",
+            "transfer": "2 جيجا ",
+            "validity": "30 ايام"
+        },
+        {
+            "price": "1000 ريال",
+            "time": "مفتوح ",
+            "transfer": "4 جيجا  ",
+            "validity": "30 ايام  "
+        }
     ],
-    "estr": "fiberlive/live.html",
+
+    // 7. العروض والخدمات
+    "offersTitle": "خدمات وعروض شبكة يمن نت 🔥",
+    "offers": "أهلاً بك زائرنا العزيز 😍\n• اتصال سريع وخدمة مميزة وتقنية متجددة\n• توصيل مودمات للمنازل\n• نسعى لتقديم أفضل خدمة إنترنت سريعة ومتميزة",
+    "offersBadge": "عرض مجاني",
+    "offersBadgeV": true,
+    "offersBtnText": "📺 مشاهدة البث المباشر والمباريات",
+    "offersBtnUrl": "http://2.2.2.2:85/",
+    "offersBtnV": true,
+    "loan-button": true,
+    "loan-text": "أهلاً بك زائرنا في شبكة يمن نت",
+
+    // 8. نقاط البيع والتوزيع
+    "salesPoints": [
+        "جميع البقالات المجاورة للشبكة"
+    ],
+    "sell-points": [
+        {
+            "name": "جميع البقالات المجاورة للشبكة"
+        }
+    ],
+
+    // 9. روابط الترفيه واستراحة الشبكة
+    "estr": "http://2.2.2.2",
     "estrV": true,
-    "moba": "https://kor.fntvs.net/channel.php",
+    "moba": "http://2.2.2.2:85/",
     "mobaV": true,
+    "redirect-to-esterahah": "http://2.2.2.2",
+    "redirect-to-mobasher": "http://2.2.2.2:85/",
+
+    // 10. إعدادات المصحف الشريف
     "quranUrl": "",
     "quranV": false,
+
+    // 11. إعدادات الكوكيز ونظام الحظر للأخطاء
     "enableHotCookie": true,
+    "enable-hot-cookie": 1,
     "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 2,
     "try-count": 5,
     "warn-when": 3,
-    "block-time": 5,
-    "warn-message": "تبقى لك {{restTryCount}} محاولات قبل أن يتم حظرك لمدة {{blockTime}} دقائق"
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+
+    // 12. إعدادات الإدخال ونوع تسجيل الدخول
+    "login-type": "",
+    "input-autocomplete": "on",
+    "input-type": "tel",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 1,
+    "price-button": true,
+    "sell-point-button": true,
+    "show-date-field": true
+};
+
+// جسر التوافق البرمجي لضمان عمل كافة الوظائف القديمة والحديثة معاً
+if (typeof window.hotspotConfig === 'object' && window.hotspotConfig !== null) {
+    Object.assign(window.hotspotConfig, window.siteConfig);
+} else {
+    window.hotspotConfig = Object.assign({}, window.siteConfig);
+}
+
+window.Config = function(cfg) {
+    if (cfg && typeof cfg === 'object') {
+        Object.assign(window.siteConfig, cfg);
+        Object.assign(window.hotspotConfig, cfg);
+    }
 };
